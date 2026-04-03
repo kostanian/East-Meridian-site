8ca5c0f2e56cde6921d9be616e4f264fc229fd47
kostanian <art2r.kostanian@gmail.com>
2026-04-03 19:29:29 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,22 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Index from "./pages/Index";
 
-const queryClient = new QueryClient();
-
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <div className="min-h-screen bg-background">
-        <Navbar />
-        <main>
-          <Index />
-        </main>
-        <Footer />
-      </div>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <TooltipProvider>
+    <Toaster />
+    <Sonner />
+    <div className="min-h-screen bg-background">
+      <Navbar />
+      <main>
+        <Index />
+      </main>
+      <Footer />
+    </div>
+  </TooltipProvider>
 );
 
 export default App;
