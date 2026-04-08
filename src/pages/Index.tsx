1304bd71b6a97dbea90b43290bad48d1607be2b7
kostanian <art2r.kostanian@gmail.com>
2026-04-08 19:58:12 -0300

    update

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -69,7 +69,7 @@ const serviceCards = [
       'Поиск производителей и проверка поставщика',
     ],
     image: dealSupportImg,
-    imagePosition: 'object-[center_30%]',
+    imagePosition: 'object-[center_15%]',
   },
   {
     title: 'Юридическая поддержка',
